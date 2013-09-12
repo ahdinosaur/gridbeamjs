1651144387c2652e312347d22e792a5cd30ab21b
Michael Williams <dinosaur@riseup.net>
2013-09-11 20:26:57 -0700

    refactored gridbeams into individual pieces

--- client/lib/gridbeam.js
+++ client/lib/gridbeam.js
@@ -17,13 +17,10 @@ hole_radius=2.4;
 // options are
 // position, scale, rotation
 
-var stick = function(length) {
+var stick = function() {
     var geometry = new THREE.CubeGeometry(
-        beam_width, beam_width, length * beam_width);
+        beam_width, beam_width, beam_width);
     var mesh = new THREE.Mesh(geometry);
-    mesh.translateX(beam_width / 2);
-    mesh.translateY(beam_width / 2);
-    mesh.translateZ((length * beam_width) / 2);
     return mesh;
 }
 
@@ -39,55 +36,43 @@ var hole = function() {
     return mesh;
 }
 
-var xHole = function (i) {
+var xHole = function () {
     mesh = hole();
+    return mesh;
+}
+
+var yHole = function () {
+    mesh = hole();
+    mesh.rotateOnAxis(new THREE.Vector3(0, 0, 1), Math.PI / 2);
+    return mesh;
+}
+
+var pieceGeometry = function() {
+    var bsp = new ThreeBSP(stick());
+    bsp = bsp.subtract(new ThreeBSP(xHole()));
+    bsp = bsp.subtract(new ThreeBSP(yHole()));
+    var geometry = bsp.toGeometry();
+    geometry.computeVertexNormals();
+    return geometry;
+}();
+var pieceMaterial = new THREE.MeshBasicMaterial({
+    shading: THREE.SmoothShading,
+    map: THREE.ImageUtils.loadTexture('img/douglas_fir.jpg')
+});
+
+var piece = function(i) {
+    var mesh = new THREE.Mesh(pieceGeometry, pieceMaterial);
     mesh.translateX(beam_width / 2);
     mesh.translateY(beam_width / 2);
     mesh.translateZ((beam_width / 2) + (i * beam_width));
-    /* {
-        start: [-1, beam_width/2, i*beam_width + beam_width/2],
-        end: [beam_width+1, beam_width/2, i*beam_width + beam_width/2],
-        radius: hole_radius,
-        resolution: cylresolution
-    } );*/
-    return mesh;
-}
-
-var yHole = function (i) {
-    mesh = hole();
-    mesh.rotateOnAxis(new THREE.Vector3(0, 0, 1), Math.PI / 2);
-    mesh.translateX( beam_width / 2);
-    mesh.translateY( - beam_width / 2);
-    mesh.translateZ((beam_width / 2) + i * beam_width);
-    /*return cylinder( {
-        start: [beam_width/2, -1, i*beam_width + beam_width/2],
-        end: [beam_width/2, beam_width+1, i*beam_width + beam_width/2],
-        radius: hole_radius,
-        resolution: cylresolution
-    } );*/
-    return mesh;
+    return mesh
 }
 
 zBeam = function (length) {
-    var stick_bsp = new ThreeBSP(stick(length));
+    var pole = new THREE.Object3D();
 
     for (var i = 0; i < length; i++) {
-        stick_bsp = stick_bsp.subtract(new ThreeBSP(xHole(i)));
-        stick_bsp = stick_bsp.subtract(new ThreeBSP(yHole(i)));
+        pole.add(piece(i));
     }
-    beam = stick_bsp.toMesh(new THREE.MeshBasicMaterial({
-        shading: THREE.SmoothShading,
-        map: THREE.ImageUtils.loadTexture('img/douglas_fir.jpg')
-    }));
-    //var holes = [];
-    //for (var i = 0; i < length; i++)
-    //{
-    //    holes.push(xHole(i));
-    //    holes.push(yHole(i)); 
-    //}
-    //var beam = cube.subtract(holes);
-    //mesh.position = THREE.Vector3.apply(options.position);
-    //mesh.rotation = THREE.Vector3.apply(options.rotation);
-    beam.geometry.computeVertexNormals();
-    return beam;
+    return pole;
 }

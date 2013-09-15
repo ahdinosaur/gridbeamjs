caa0acb8ade58af92a15c440a1f0fcdec673a4a1
Michael Williams <dinosaur@riseup.net>
2013-09-14 21:39:09 -0700

    continue implementing initial project creation

--- client/main.js
+++ client/main.js
@@ -1,22 +1,48 @@
 Router.configure({
   notFoundTemplate: 'notFound',
   loadingTemplate: 'loading',
-  layout: 'browser',
+  layout: 'layout',
   renderTemplates: {
-    'browserNavbar': { to: 'navbar'}
+    'container': { to: 'container' },
+    'navbar': { to: 'navbar' }
   }
-})
+});
 
 Router.map(function () {
   this.route('home', {
     path: '/'
   });
-  this.route('edit', {
-    path: '/edit',///:id',
-    //data: function () { return Projects.findOne(this.params._id); },
-    layout: 'editor',
+  this.route('new', {
+    path: '/new',
+    controller: 'NewProjectController'
+  });
+  this.route('create', {
+    path: '/create/:id',
+    data: function () { return Projects.findOne({ _id: this.params.id }); },
     renderTemplates: {
-      'editorNavbar': { to: 'navbar' }
+      'null': { to: 'container'},
+      'createNavbar': { to: 'navbar' }
     }
   });
+  this.route('explore', {
+    path: '/explore',
+    data: function () { return Projects.find({}); },
+    renderTemplates: {
+      'null': { to: 'container'},
+      'exploreNavbar': { to: 'navbar' }
+    }
+  });
+});
+
+Meteor.startup(function () {
+  Hooks.init();
+  Hooks.onLoggedIn = function () {
+    var userId = Meteor.userId();
+    if (userId) {
+      Session.get('creations').forEach(function (projId) {
+        Projects.update({ _id: projId }, { $set: { author: userId } });
+      });
+      Session.set('creations', null);
+    }
+  };
 });
--- collections/projects.js
+++ collections/projects.js
@@ -1,1 +1,15 @@
 Projects = new Meteor.Collection('projects');
+
+defaultProjectTitle = "Untitled Project";
+
+if (Meteor.isServer) {
+  Meteor.publish('projects', function () {
+    return Projects.find({
+      $or: [
+        { public: true },
+        { author: Meteor.userId() }
+      ]
+    });
+  });
+}
+

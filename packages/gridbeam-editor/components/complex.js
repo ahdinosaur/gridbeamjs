e74b40ea2965a19216fb6a6977ae20b3c1929ccb
Michael Williams <michael.williams@enspiral.com>
2019-07-30 22:57:35 -0700

    revert back to geometry, simplify, and now normals work!

--- packages/gridbeam-editor/components/complex.js
+++ packages/gridbeam-editor/components/complex.js
@@ -1,57 +1,34 @@
 const React = require('react')
 const THREE = require('three')
-const Normals = require('normals')
 const { useUpdate } = require('react-three-fiber')
 
 module.exports = SimplicialComplexGeometry
 
 function SimplicialComplexGeometry (props) {
   const { mesh, attach } = props
-  var { positions, cells } = mesh
-
-  console.log('mesh', mesh)
+  const { positions, cells } = mesh
 
   const ref = useUpdate(
     geometry => {
-      updateCells(geometry, cells)
       updatePositions(geometry, positions)
-      updateNormals(geometry, cells, positions)
-      console.log('geometry', geometry)
+      updateCells(geometry, cells, positions)
+      geometry.computeFaceNormals()
+      geometry.computeVertexNormals()
     },
     [mesh]
   )
 
-  return <bufferGeometry attach={attach} ref={ref} />
-}
-
-function updateCells (geometry, cells) {
-  geometry.setIndex(toBufferAttribute(cells, Uint32Array))
-  geometry.index.needsUpdate = true
+  return <geometry attach={attach} ref={ref} />
 }
 
 function updatePositions (geometry, positions) {
-  geometry.addAttribute('position', toBufferAttribute(positions, Float32Array))
-  geometry.attributes.position.needsUpdate = true
+  geometry.vertices = positions.map(pos => new THREE.Vector3().fromArray(pos))
+  geometry.verticesNeedUpdate = true
 }
 
-function updateNormals (geometry, cells, positions) {
-  const faceNormals = Normals.vertexNormals(cells, positions)
-  geometry.addAttribute('normal', toBufferAttribute(faceNormals, Float32Array))
-  geometry.attributes.normal.needsUpdate = true
-}
-
-// TODO use flatten-vertex-data
-function toBufferAttribute (items, TypedArray) {
-  const itemSize = items[0].length
-  const bufferLength = itemSize * items.length
-
-  var bufferArray = new TypedArray(bufferLength)
-
-  for (var i = 0, k = 0; i < items.length; i++) {
-    for (var j = 0; j < itemSize; j++) {
-      bufferArray[k++] = items[i][j]
-    }
-  }
-
-  return new THREE.BufferAttribute(bufferArray, itemSize)
+function updateCells (geometry, cells, positions) {
+  geometry.faces = cells.map((cell, index) => {
+    return new THREE.Face3(cell[0], cell[1], cell[2])
+  })
+  geometry.elementsNeedUpdate = true
 }

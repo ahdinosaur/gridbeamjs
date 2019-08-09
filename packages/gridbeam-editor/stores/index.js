3e916111fc88549aa396371ec077a7efac4c121d
Michael Williams <michael.williams@enspiral.com>
2019-08-08 18:55:28 -0700

    mad stores

--- packages/gridbeam-editor/stores/index.js
+++ packages/gridbeam-editor/stores/index.js
@@ -3,8 +3,80 @@ import create from 'zustand'
 
 export default base
 
-function base (config) {
+//  new store
+//  - single zustand store
+//  - many "dino" stores
+//
+//  - name
+//  - actions: {
+//      action: (set) => (...args) => set(state => {
+//        state.key = value
+//      })
+//    }
+//  - selectors: {
+//      select: [
+//        name,
+//        (...values) => selection
+//      ]
+//  - effects: {
+//      react: [
+//        name,
+//        (...values) => action(...args) | null
+//      ]
+//    }
+
+function dino (config) {
+  return function nextConfig(set, get, api) {
+    const {
+      actions,
+      selectors,
+      effects
+    } = config
+
+    var store = [
+    ]
+
+    forEach(actions, (action, name) => {
+      store.actions = 
+    })
+    store.actions
+
+    return store
+
+
+    var connectedSelectors = mapValues(selectors, ([...deps, select], key) => (state) => {
+      var depValues = map(deps, pipe(prop, useStore)) // name => useStore(prop(name))
+    })
+  }
+}
+
+function dino (configs) {
+  var store = {}
+  configs.forEach(config => {
+    const {
+      name,
+      actions,
+      selectors,
+      effects
+    } = config
+
+    store[name] = create(actions)
+    forEach((select, name) => {
+      const [...args, handler] = select
+
+    }, selectors)
+    reactors[name] = 
+    store[name][2].subscribe(
+
+    ([...values(actions), ...values(selectors), ...values(effects)]).forEach(handler => handler(set, get, api))
+      
+    })
+    var path = [name, type, name]
+  })
   return create(log(immer(config)))
+
+  function config(set, get, api) {
+  }
 }
 
 function log (config) {
